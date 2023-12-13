6ee18f3949ae946022886c3879b3964eb6ff6146
JDazogbo <jdazo054@uottawa.ca>
2023-12-13 14:28:16 -0500

    Added Configuration Parameters to program

--- StartDemo.py
+++ StartDemo.py
@@ -1,8 +1,35 @@
+#To get the list of com ports: python -m serial.tools.miniterm
+
 import serial
 
-userPort = serial.Serial('/dev/ttyACM0', 115200) # User Port
+PROFILE_PATH = 'profile.cfg'
 
-userPort.write(b'\n')
+userPort = serial.Serial('/dev/ttyACM0', 115200) # User Port
+dataPort = serial.Serial('/dev/ttyACM1', 921600)
+
+
+userPort.write(b'%\n')
 msg = userPort.read(11) #This methods reads up to 11 characters which are the characters for "\nmmwDemo:/>"
-userPort.close()
 print(msg)
+
+try:
+    with open(PROFILE_PATH, 'r') as file:
+        lines = file.readlines()
+
+except Exception as e:
+    print("The Configuration File '" + PROFILE_PATH + "' was not found")
+
+else:
+    for line in lines:
+        userPort.write(line.encode('utf-8'))
+        byteCount = userPort.inWaiting()
+        s = userPort.read(byteCount)
+        print(s)
+    
+    userPort.write(b'sensorStart')
+
+    while True:
+        byteCount = dataPort.inWaiting()
+        s = dataPort.read(byteCount)
+        print(s)
+
