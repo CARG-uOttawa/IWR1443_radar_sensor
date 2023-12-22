0f4c49a9b01db575e054ad5ab040161ae4a19605
JDazogbo <jdazo054@uottawa.ca>
2023-12-22 16:43:59 -0500

    Combined demo reader with parser

--- StartDemo.py
+++ StartDemo.py
@@ -1,38 +1,59 @@
-#To get the list of com ports: python -m serial.tools.miniterm
-
 import serial
-
-PROFILE_PATH = 'profile.cfg'
-
-userPort = serial.Serial('/dev/ttyACM0', 115200) # User Port
-dataPort = serial.Serial('/dev/ttyACM1', 921600)
+import time
+import numpy as np
+from UARTParser import UartParser as up
 
 
-# userPort.write(b'%\n')
-# msg = userPort.read(11) #This methods reads up to 11 characters which are the characters for "\nmmwDemo:/>"
-# print(msg)
+if __name__ == "__main__":
+    PROFILE_PATH = 'profile.cfg'
 
-try:
-    with open(PROFILE_PATH, 'r') as file:
-        lines = file.readlines()
+    userPort = serial.Serial('/dev/ttyACM0', 115200)  # User Port
+    dataPort = serial.Serial('/dev/ttyACM1', 921600)
 
-except Exception as e:
-    print("The Configuration File '" + PROFILE_PATH + "' was not found")
+    try:
+        print("Opening configuration file")
+        with open(PROFILE_PATH, 'r') as file:
+            lines = file.readlines()
+        print("Configuration was opened successfully")
 
-else:
-    for line in lines:
-        userPort.write(line.encode('utf-8'))
+    except Exception as e:
+        print("The Configuration File '" + PROFILE_PATH + "' was not found")
+
+    else:
+        print("Loading Configuration to board")
+        for line in lines:
+            time.sleep(0.001)
+            userPort.write(line.encode('utf-8'))
+            byteCount = userPort.inWaiting()
+            s = userPort.read(byteCount)
+
+        userPort.write(b'\n')  # We add this \n since the startSensor instruction in the .cfg file doesn't have one
         byteCount = userPort.inWaiting()
         s = userPort.read(byteCount)
-        print(s)
-    
-    userPort.write(b'\n') # We add this \n since the startSensor instruction in the .cfg file doesn't have one 
-    byteCount = userPort.inWaiting()
-    s = userPort.read(byteCount)
-    print(s)
 
-    while True:
-        byteCount = dataPort.inWaiting()
-        s = dataPort.read(byteCount)
-        print(s)
+        print("Configuration was loaded successfully")
 
+        data = np.array([])
+        try:
+            print("Reading Data from board. Press Ctrl+C to exit and save data.")
+            while True:
+                byteCount = dataPort.inWaiting()
+                s = np.frombuffer(dataPort.read(byteCount), dtype='uint8')
+                if s.any():
+                    data = np.concatenate((data, s), axis=0)
+
+        except KeyboardInterrupt:
+            print("\nData read successfully")
+
+        except Exception:
+            print("An error occurred while reading the Data")
+        try:
+            print("Processing Data")
+            indices = up.searchMagicWord(data)
+            objectData = []
+            for index in range(indices.size-2):
+                objectData.append(up.convertData(data[indices[index]:indices[index+1]]))
+            print("Data was processed")
+
+        except Exception:
+            print("An error occurred while processing the Data")
